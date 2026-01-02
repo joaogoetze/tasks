fdaad756a8c29cd6000f8b26c5001a0f8d4625a4
joaogoetze <joaoaugustoferreira5@gmail.com>
2026-01-02 19:05:07 -0300

    disabling the backgorund color of the top bar

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.tasks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -19,7 +18,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
 
             val navController = rememberNavController()
